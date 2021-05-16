4bc3993e3e53e0b7154a9b72cee51f35e727bed4
forsbergmaria <maria@mariaforsberg.com>
2021-05-16 14:35:01 +0200

    Update private.xml

--- src/mib/application/ValideringInloggning.java
+++ src/mib/application/ValideringInloggning.java
@@ -4,7 +4,10 @@
 package mib.application;
 
 import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import oru.inf.InfDB;
+import oru.inf.InfException;
 
 /**
  *
@@ -12,6 +15,7 @@ import javax.swing.JTextField;
  */
 public class ValideringInloggning {
     
+    private static InfDB idb;
  
      //En metod för att kolla om rutan för ID-nummer är tom eller inte
     
@@ -24,6 +28,52 @@ public class ValideringInloggning {
              resultat = false;
          }
         return resultat;
+    }
+    
+    public static boolean korrektLosenord(JTextField idNummerRuta, JPasswordField losenordRuta)
+    {
+        boolean resultat = false;
+        
+        try {
+        String idNummer = idNummerRuta.getText();
+        String hamtaLosenord = "SELECT Losenord FROM Agent WHERE Agent_ID =" + idNummer;
+        String losenordFraga = idb.fetchSingle(hamtaLosenord);
+        String losenord=String.valueOf(losenordRuta.getPassword());
+
+       
+        if(losenord.equals(losenordFraga)) {
+            resultat = true;
+                                           }
+        
+            else {
+            JOptionPane.showMessageDialog(null, "Felaktigt lösenord eller ID-nummer. Vänligen försök igen");
+                   
+                 }
+            }
+        
+        catch(Exception e) {}
+    
+        return resultat;
+    }
+    
+    public static boolean isAdmin(JTextField idNummerRuta)
+    {
+        boolean resultat = false;
+            String idNummer = idNummerRuta.getText();
+            String hamtaAdminStatus = "SELECT Administrator FROM Agent WHERE Agent_ID =" + idNummer;
+        
+            try{
+                String adminStatusFraga = idb.fetchSingle(hamtaAdminStatus);
+                String adminStatus = String.valueOf(adminStatusFraga);
+               
+            
+                if(adminStatus.equals("J")){
+                    resultat = true;
+                }
+            }
+                catch(Exception e) {}
+        
+                    return resultat;
     }
     
 }
